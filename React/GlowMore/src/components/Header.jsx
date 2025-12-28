212b127938f8be4b763903a2187e6d1faa78d8da
Raman-Meena <meenaraman687@gmail.com>
2025-12-28 16:41:27 +0530

    Glow More - Makeup App

--- React/GlowMore/src/components/Header.jsx
+++ React/GlowMore/src/components/Header.jsx
@@ -1,15 +1,16 @@
 import React from "react"
 import {Link} from "react-router-dom"
 import { IoHomeSharp } from "react-icons/io5";
+import { IoIosInformationCircle } from "react-icons/io";
 
 const Header = () => {
   return (
     <>
       <div className="flex justify-between p-2 align-ite bg-amber-400">
-        <h3 className="font-bold text-2xl">My Website</h3>
-        <div className="flex gap-3">
-           <Link to={"/"} className="font-bold hover:text-white"><IoHomeSharp />Home</Link>
-           <Link to={"/about"} className="font-bold hover:text-white">About</Link>
+        <h3 className="font-bold text-2xl">GlowMore</h3>
+        <div className="flex gap-5 items-center">
+           <Link to={"/"} className="font-bold hover:text-white flex gap-1 items-center"><IoHomeSharp />Home</Link>
+           <Link to={"/about"} className="font-bold hover:text-white flex gap-1 items-center"><IoIosInformationCircle />About</Link>
         </div>
       </div>
     </>
