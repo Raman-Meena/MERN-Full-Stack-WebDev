88e6c744e4c657b678b7688a4963524de661b9b2
Raman-Meena <meenaraman687@gmail.com>
2026-01-06 20:36:52 +0530

    Basic Backend and First Backend Practice

---
+++ Node/Basic/index.js
@@ -0,0 +1,17 @@
+console.log("Hello World!");
+
+function add(a, b){
+    console.log(a+b);  
+}
+
+function sub(a, b){
+    console.log(a-b);  
+}
+
+function mul(a, b){
+    console.log(a*b);  
+}
+
+console.log("abcd");
+
+export default mul;
---
+++ Node/Basic/index2.js
@@ -0,0 +1,7 @@
+import add from './index.js'
+
+let x = 10;
+let y = 20;
+
+add(x, y);
+// sub(y, x);
