e96d6593e46ed2de630ebb1c0d61d1016d4981db
Raman-Meena <meenaraman687@gmail.com>
2026-01-09 21:01:40 +0530

    Backend connected to MongoDB

--- Node/FirstBackend/server.js
+++ Node/FirstBackend/server.js
@@ -2,16 +2,22 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import connectDB from "./src/config/db.js";
+import AuthRouter from "./src/routers/myrouter.js";
 
 const app = express();
 
+app.use(express.json());
+
+app.use("/auth", AuthRouter);
+
 app.get("/", (req, res) => {
   console.log("Server is Running");
-  res.json({message:"Server is Running Successfully"})
+  res.json({ message: "Server is Running Successfully" });
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("Server started at port ", port);
+  connectDB();
 });
-
