db16f11a747491fcd4c51e1725fa5885d79e9136
Raman-Meena <meenaraman687@gmail.com>
2026-01-15 19:58:26 +0530

    Cravings - A Zomato Clone

---
+++ Cravings/server/src/controllers/publicController.js
@@ -0,0 +1,31 @@
+import Contact from "../models/contactModel.js";
+
+export const NewContact = async (req, res, next) => {
+  try {
+    const { fullName, email, mobileNumber, message } = req.body;
+
+    if (!fullName || !email || !mobileNumber || !message) {
+      const error = new Error("All feilds required");
+      error.statusCode = 400;
+      return next(error);
+    }
+
+    const newContact = await Contact.create({
+      fullName,
+      email,
+      mobileNumber,
+      message,
+    });
+
+    console.log(newContact);
+
+    res
+      .status(201)
+      .json({
+        message:
+          "Thank you for Contacting us",
+      });
+  } catch (error) {
+    next(error);
+  }
+};
