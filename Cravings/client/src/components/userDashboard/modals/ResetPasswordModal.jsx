8241eabbeff12ed23b5a2966b7d611f4f661cfb0
Raman-Meena <meenaraman687@gmail.com>
2026-01-30 20:54:01 +0530

    Cravings  - A Zomato Clone

---
+++ Cravings/client/src/components/userDashboard/modals/ResetPasswordModal.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const ResetPasswordModal = ({onClose}) => {
+    const [formData, setFormData] = useState({})
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default ResetPasswordModal
